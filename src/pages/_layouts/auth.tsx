6ae77e420e5865661d6a7ee3c2d5403e48440e23
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2024-12-25 12:10:56 -0300

    corrigindo rotas

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -5,7 +5,7 @@ export function AuthLayout() {
   return (
     <div className="main-h-screen grid grid-cols-2">
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
-        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
           <span className="font-semibold">pizza.shop</span>
         </div>
