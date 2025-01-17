991d078cff7712a3294ac425734326b8524971c0
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-01-17 20:56:37 -0300

    Receita no período

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -6,14 +6,6 @@ import colors from 'tailwindcss/colors'
 import { getPopularProducts } from '@/api/get-popular-products'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
-const data = [
-  { product: 'Peperoni', amount: 30 },
-  { product: 'Mussarela', amount: 40 },
-  { product: 'Marguerita', amount: 50 },
-  { product: '4 Queijos', amount: 16 },
-  { product: 'Frango frito', amount: 26 },
-]
-
 const COLORS = [
   colors.sky[500],
   colors.amber[500],
