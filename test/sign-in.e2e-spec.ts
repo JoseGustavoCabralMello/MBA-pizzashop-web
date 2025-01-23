df17b0f49de731b61d64cd2ffe44dca56e507637
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-01-22 22:12:01 -0300

    Async Assertions e encerramento

--- test/sign-in.e2e-spec.ts
+++ test/sign-in.e2e-spec.ts
@@ -10,7 +10,7 @@ test('sig in successfully', async ({ page }) => {
     'Enviamos um link de autenticação no seu e-mail.',
   )
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 })
 
 test('sig in with wrong credentials', async ({ page }) => {
@@ -21,7 +21,7 @@ test('sig in with wrong credentials', async ({ page }) => {
 
   const toast = page.getByText('Credenciais inválidas.')
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 })
 
 test('navigate to new restaurant page', async ({ page }) => {
